302a7d5158d77289ec8d7a6f2fd82151f9839bf4
Pavel Razuvayev <prx124@my.utsa.edu>
2015-09-27 15:05:22 -0500

    add account activations through email

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User",
+                     email: "user@example.com",
+                     password: "foobar",
+                     password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -61,5 +64,9 @@ class UserTest < ActiveSupport::TestCase
   test "password should have a min length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
+  end
+
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
   end
 end
